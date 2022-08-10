2c3db670cbfeb1123992a15f7eaae79060b4a3f5
chaiqou <lomtadzenikusha@gmail.com>
2022-08-10 17:08:48 +0400

    markup: Main Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
+  <MainNav />
   <div class="text-red-400">Hello Vue</div>
 </template>
 
-<script setup></script>
+<script setup>
+import MainNav from "@/components/ui/MainNav.vue";
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "talga-gray-1": "#dadce0",
+        "talga-green-1": "#137333",
+        "talga-blue-1": "#1967d2",
       },
     },
   },

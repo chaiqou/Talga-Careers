72afb0b2a80493cb5659adb54fb5e50381ad9bfb
chaiqou <lomtadzenikusha@gmail.com>
2022-08-13 19:48:23 +0400

    chore: use @apply tailwind directive and disable css warnings

--- src/components/ui/ActionButton.vue
+++ src/components/ui/ActionButton.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <button
-      class="px-5 py-3 font-medium text-white rounded-md bg-talga-blue-1 hover:shadow-talga-shadow-1"
-    >
-      Sign In
-    </button>
+    <button class="primary">Sign In</button>
   </div>
 </template>
 
 <script setup></script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 rounded font-medium;
+}
+
+.primary {
+  @apply text-white bg-talga-blue-1 hover:shadow-talga-shadow-1;
+}
+
+.secondary {
+}
+</style>

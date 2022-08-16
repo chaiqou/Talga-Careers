109839cd94d652488539a4969da96f348b2f264e
chaiqou <lomtadzenikusha@gmail.com>
2022-08-16 11:37:22 +0400

    chore: Use Fontawesome global component in vue

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="w-full h-16 bg-white border-b border-solid border-talga-gray-1">
-    <div class="flex items-center h-full px-9">This is a subnav</div>
+    <div class="flex items-center h-full px-9">
+      <div>
+        <font-awesome-icon
+          :icon="['fas', 'search']"
+          class="mr-3 text-gray-500 hover:text-gray-700"
+        />
+        <span> <span class="text-talga-green-1">3264</span> jobs matched </span>
+      </div>
+    </div>
   </div>
 </template>
 

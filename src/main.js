bbc7a9b691cfb9e55cdd8d3c7a65965a61f8d690
chaiqou <lomtadzenikusha@gmail.com>
2022-08-16 11:28:13 +0400

    feat: Register global component font-awesome-icon

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import App from "@/App.vue";
 import "@/main.css";
 
+library.add(faSearch);
+
 const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(createPinia());
 app.mount("#app");

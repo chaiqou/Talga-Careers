b733d01621527bd86f12d306712719e017964454
chaiqou <lomtadzenikusha@gmail.com>
2022-08-23 11:59:55 +0400

    chore: divide job listings file

---
+++ src/components/jobresults/JobListing.vue
@@ -0,0 +1,33 @@
+<template>
+  <li class="mb-7">
+    <router-link
+      to="/job-results/1"
+      class="block mx-auto bg-white border border-solid border-talga-gray-2 rounded-xl hover:shadow-xl hover:shadow-gray-200"
+    >
+      <div class="pt-5 pb-2 mx-8 border-b border-solid border-talga-gray-2">
+        <h2 class="mb-2 text-2xl">Software Engineer</h2>
+
+        <div class="flex flex-row align-middle">
+          <div class="mr-5">
+            <span>Talga</span>
+          </div>
+          <div>
+            <span>Georgia</span>
+          </div>
+        </div>
+      </div>
+      <div class="px-8 py-4">
+        <div>
+          <h3 class="mt-1 mb-2">Qualifications:</h3>
+          <div>
+            <ul class="pl-8 list-disc">
+              <li>Bachelors degree</li>
+              <li>6+ experience in programming</li>
+              <li>Laravel Vue experience</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </li>
+</template>
--- src/components/jobresults/JobListings.vue
+++ src/components/jobresults/JobListings.vue
@@ -1,27 +1,11 @@
 <template>
   <div class="flex-auto p-8 bg-talga-gray-2">
     <ol>
-      <li class="mb-7">
-        <router-link
-          to="/job-results/1"
-          class="block mx-auto bg-white border border-solid border-talga-gray-2 rounded-xl hover:shadow-xl hover:shadow-gray-200"
-        >
-          <div class="pt-5 pb-2 mx-8 border-b border-solid border-talga-gray-2">
-            <h2 class="mb-2 text-2xl">Software Engineer</h2>
-
-            <div class="flex flex-row align-middle">
-              <div class="mr-5">
-                <span>Talga</span>
-              </div>
-              <div>
-                <span>Georgia</span>
-              </div>
-            </div>
-          </div>
-        </router-link>
-      </li>
+      <JobListing />
     </ol>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import JobListing from "./JobListing.vue";
+</script>

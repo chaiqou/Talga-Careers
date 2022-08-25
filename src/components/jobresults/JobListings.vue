a4165073f0db94c8c55d5292cf76c993c48812a1
chaiqou <lomtadzenikusha@gmail.com>
2022-08-25 13:39:07 +0400

    feat: create algorith wich dynamically  displays job results by 10

--- src/components/jobresults/JobListings.vue
+++ src/components/jobresults/JobListings.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flex-auto p-8 bg-talga-gray-2">
+  <div class="flex-auto p-8 bg-[#E5E5E5]">
     <ol>
-      <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+      <JobListing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
+import { useRoute } from "vue-router";
 import JobListing from "@/components/jobresults/JobListing.vue";
 import axiosInstance from "@/config/axios";
 
@@ -21,4 +22,16 @@ const fetchJobs = async () => {
   const response = await axiosInstance.get("api/jobs");
   jobs.value = response.data.data;
 };
+
+const displayedJobs = computed(() => {
+  const route = useRoute();
+
+  const pageString = route.query.page || "1";
+  const pageNumber = Number.parseInt(pageString);
+
+  const firstJobIndex = (pageNumber - 1) * 10;
+  const lastJobIndex = pageNumber * 10;
+
+  return jobs.value.slice(firstJobIndex, lastJobIndex);
+});
 </script>

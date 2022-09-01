ea65dc9ef7a6a834b28515001a6c9f4adeaf7c93
chaiqou <lomtadzenikusha@gmail.com>
2022-09-01 10:40:45 +0400

    feat: create spotlight card component

---
+++ src/components/jobsearch/Spotlight.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-bind:key="spotlight" v-for="spotlight in spotlights">
+      <slot :spotlight="spotlight"></slot>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import axiosInstance from "@/config/axios";
+import { ref, onMounted } from "vue";
+const spotlights = ref([]);
+
+onMounted(async () => {
+  const response = await axiosInstance.get("api/posts");
+  spotlights.value = response.data.data;
+});
+</script>

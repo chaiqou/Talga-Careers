d220c2be6aa20c6eadb65d8faea2770a3c7d6a1d
chaiqou <lomtadzenikusha@gmail.com>
2022-09-02 12:58:34 +0400

    feat: create getter for unique organizations

--- src/stores/useLoginStore.js
+++ src/stores/useLoginStore.js
@@ -1,8 +1,17 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 export const useLoginStore = defineStore("useLoginStore", () => {
+  // State
   const isLoggedIn = ref(false);
+  const jobs = ref([]);
 
-  return { isLoggedIn };
+  // Getters
+  const uniqueOrganizations = computed(() => {
+    const unique = new Set();
+    jobs.value.forEach((job) => unique.add(job));
+    return unique;
+  });
+
+  return { isLoggedIn, uniqueOrganizations };
 });

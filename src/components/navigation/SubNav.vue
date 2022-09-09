f0bbfcdd9b13c4a750c3ae0601deb328d05e4ce4
chaiqou <lomtadzenikusha@gmail.com>
2022-09-09 13:12:57 +0400

    feat: display existing jobs number

--- src/components/navigation/SubNav.vue
+++ src/components/navigation/SubNav.vue
@@ -6,10 +6,23 @@
           :icon="['fas', 'search']"
           class="mr-3 text-gray-500 hover:text-gray-700"
         />
-        <span> <span class="text-talga-green-1">3264</span> jobs matched </span>
+        <span>
+          <span class="text-talga-green-1">{{
+            FILTERED_JOBS_BY_ORGANIZATIONS.length
+          }}</span>
+          jobs matched
+        </span>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useStore } from "vuex";
+import { computed } from "vue";
+const store = useStore();
+
+const FILTERED_JOBS_BY_ORGANIZATIONS = computed(
+  () => store.getters.FILTERED_JOBS_BY_ORGANIZATIONS
+);
+</script>
